8a51e58ab4d39a971ed7139dda62b4de4ea8049d
mrezkialqadri <106749563+mrezkialqadri@users.noreply.github.com>
2022-06-02 22:11:51 +0700

    Update UasPrakai.py

--- UasPrakai.py
+++ UasPrakai.py
@@ -1,34 +1,3 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "name": "Untitled2.ipynb",
-      "provenance": [],
-      "collapsed_sections": [],
-      "authorship_tag": "ABX9TyPmg3oxZKdgDDMRGZpQaylq",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/mrezkialqadri/Uas-prk-ai/blob/main/UasPrakai.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
       "cell_type": "code",
       "source": [
         "import numpy as np\n",
